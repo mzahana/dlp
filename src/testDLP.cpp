2bc4829261f51bdbc4b1f4fec40d401607f6106d
imoe <lym602@126.com>
2017-04-27 23:32:41 +0930

    change timing function

--- src/testDLP.cpp
+++ src/testDLP.cpp
@@ -5,10 +5,13 @@ int main(){
 
 	MatrixXi m(3,1);
 	m(0,0)=2; m(1,0)=6; m(2,0)=7;
+	problem.set_nCols(6); 
+	problem.set_nRows(7);
+	problem.set_Tp(2);  
 	problem.set_d_current_locations(m);
 	problem.set_BaseRef(3, m);
 	problem.setup_problem();
 	problem.solve_simplex();
-	//problem.solve_intp();
+//	problem.solve_intp();
 	return 0;
 }
