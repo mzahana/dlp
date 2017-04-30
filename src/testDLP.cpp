c79cf17835df6a927bcb7a8b815dad7177fb1313
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2017-05-01 02:13:57 +0300

    update test file

--- src/testDLP.cpp
+++ src/testDLP.cpp
@@ -10,7 +10,9 @@ int main(){
 	int nB=1;
 	MatrixXf Base(nB,1);
 	MatrixXf eloc(Ne,1);
+	MatrixXf dloc(Nd,1);
 	MatrixXf m(3,1);
+	MatrixXf next_loc; /* next optimal locations */
 	clock_t start, end;
 
 	m(0,0)=2.0; m(1,0)=8.0; m(2,0)=9.0;
@@ -19,16 +21,16 @@ int main(){
 	problem.set_nRows(cols);
 	problem.set_Tp(Tp);
 
-	problem.set_d_current_locations(m);
+	dloc(0,0)=4.0; dloc(1,0)=5.0; dloc(2,0)=9.0;
 
-	eloc(0,0)=30;eloc(1,0)=31;eloc(2,0)=32;
+	eloc(0,0)=15.0;eloc(1,0)=16.0;eloc(2,0)=17.0;
 
 
 	problem.set_BaseRef(nBaseref, m);
 	Base(0,0)=1.0;
 	problem.set_Base(nB, Base);
 
-	problem.set_d_current_locations(m);
+	problem.set_d_current_locations(dloc);
 	problem.set_e_current_locations(eloc);
 
 	problem.setup_problem();
@@ -36,14 +38,20 @@ int main(){
 
 	start = clock();
 	// each run typically requires the following 4 lines
-	problem.set_d_current_locations(m);
+	problem.set_d_current_locations(dloc);
 	problem.set_e_current_locations(eloc);
 	problem.update_LP();
-	//problem.solve_simplex();
-	problem.solve_intp();
+	problem.solve_simplex();
+	//problem.solve_intp();
+	problem.extract_solution();
+	problem.get_d_next_locations(next_loc);
 	end = clock();
-	if (problem.DEBUG)
+	if (problem.DEBUG){
 		cout << "Problem solved in Total time= " << (end-start)/( (clock_t)1000 ) << " miliseconds. " << endl;
+		cout << "attacker current locations: " << eloc.transpose()<< endl;
+		cout << "Defenders current locations: " << dloc.transpose() << endl;
+		cout << "Defenders next locations: " << next_loc.transpose() << endl;
+	}
 
 	return 0;
 }
