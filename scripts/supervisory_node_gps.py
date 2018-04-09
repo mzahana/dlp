d110caf381ba28630001e652afba0083783b4da1
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2018-04-09 14:23:26 +0300

    updates for outdoors test

--- scripts/supervisory_node_gps.py
+++ scripts/supervisory_node_gps.py
@@ -6,7 +6,7 @@ from std_msgs.msg import *
 from gazebo_msgs.msg  import ModelStates
 from geometry_msgs.msg import Point, Point32, PoseStamped, Quaternion, Pose
 from sensor_msgs.msg import NavSatFix
-from dlp.msg import DefendersState, EnemyState, MasterCommand
+from dlp.msg import DefendersState, EnemyState, MasterCommand, DlpState
 from math import sqrt, ceil
 import helpers as hp
 
@@ -77,6 +77,7 @@ class MasterC():
 		self.outdoor = rospy.get_param('outdoor', False)
 
 		self.d_gps_topic_names = []
+		self.d_dlp_state_topics = []
 		for i in range(self.Nd):
 			if self.outdoor:
 				dstr = '/defender_'
@@ -86,6 +87,7 @@ class MasterC():
 				estr = '/attacker'
 
 			self.d_gps_topic_names.append(dstr+str(i+1)+'/mavros/global_position/raw/fix')
+			self.d_dlp_state_topics.append(dstr+str(i+1)+'/dlp_state')
 
 		self.e_gps_topic_names = []
 		for i in range(self.Ne):
@@ -106,6 +108,9 @@ class MasterC():
 		for i in range(self.Nd):
 			self.d_msg.defenders_position.append(p)
 			self.d_msg.defenders_sectors.append(i)
+			self.d_msg.defenders_next_sectors.append(i)
+			self.d_msg.successful_prediction_rate.append(0.0)
+			self.d_msg.connectivity_rate.append(0.0)
 
 		self.e_msg.header.stamp = rospy.Time.now()
 		self.e_msg.enemy_count = self.Ne
@@ -120,6 +125,15 @@ class MasterC():
 
 		self.master_msg.allCaptured = False
 		self.master_msg.gameEnd = False
+
+		# Defenders predictions about their neighbors
+		# List of lists
+		self.d_predictions=[]
+
+		# initialization
+		x=[0]*self.Nd
+		for i in range(self.Nd):
+			self.d_predictions.append(x)
 
 		# loop rate
 		self.rate = rospy.Rate(50)
@@ -140,6 +154,14 @@ class MasterC():
 		# get sector
 		self.d_msg.defenders_sectors[i] = self.enu2sector(x_enu, y_enu, 0.0)
 
+	def d1dlpCb(self, msg):
+		i=0
+		# next sector
+		self.d_msg.defenders_next_sectors[i] = msg.my_next_sector
+
+		# defender predicition about its neighbors
+		self.d_predictions[i] = msg.estimated_neighbors_next_locations
+
 
 	def d2poseCb(self, msg):
 		i=1
@@ -157,6 +179,14 @@ class MasterC():
 		# get sector
 		self.d_msg.defenders_sectors[i] = self.enu2sector(x_enu, y_enu, 0.0)
 
+	def d2dlpCb(self, msg):
+		i=1
+		# next sector
+		self.d_msg.defenders_next_sectors[i] = msg.my_next_sector
+
+		# defender predicition about its neighbors
+		self.d_predictions[i] = msg.estimated_neighbors_next_locations
+
 
 	def d3poseCb(self, msg):
 		i=2
@@ -173,6 +203,14 @@ class MasterC():
 
 		# get sector
 		self.d_msg.defenders_sectors[i] = self.enu2sector(x_enu, y_enu, 0.0)
+
+	def d3dlpCb(self, msg):
+		i=2
+		# next sector
+		self.d_msg.defenders_next_sectors[i] = msg.my_next_sector
+
+		# defender predicition about its neighbors
+		self.d_predictions[i] = msg.estimated_neighbors_next_locations
 
 	def e1poseCb(self, msg):
 		i=0
@@ -325,6 +363,26 @@ class MasterC():
 					rospy.logwarn('An attacker WON! Defenders should land now. Attacker will keep in the air!')
 					return
 
+	def update_prediciton_rate(self):
+		for a in range(self.Nd):
+			# number of neighbors for defender a
+			di_pred = self.d_predictions[a]
+			N = sum(x>0 for x in di_pred)
+
+			# find matches
+			matches = [i for i, j in zip(self.d_msg.defenders_next_sectors, di_pred) if i == j]
+			# number of matches
+			nMatches = len(matches)
+
+			# compute prediction rate
+			if N > 0:
+				self.d_msg.successful_prediction_rate[a] = float(nMatches)/float(N)
+			else:
+				self.d_msg.successful_prediction_rate[a] = -1.0
+
+			self.d_msg.connectivity_rate[a] = float(N)/float(self.Nd)
+		return
+
 def main():
 
 	rospy.init_node('sim_supervisory_node', anonymous=True)
@@ -347,12 +405,17 @@ def main():
 	rospy.Subscriber(mObj.d_gps_topic_names[1], NavSatFix, mObj.d2poseCb)
 	rospy.Subscriber(mObj.d_gps_topic_names[2], NavSatFix, mObj.d3poseCb)
 
+	# subscribe to defenders dlp_state
+	rospy.Subscriber(mObj.d_dlp_state_topics[0], DlpState, mObj.d1dlpCb)
+	rospy.Subscriber(mObj.d_dlp_state_topics[1], DlpState, mObj.d2dlpCb)
+	rospy.Subscriber(mObj.d_dlp_state_topics[2], DlpState, mObj.d3dlpCb)
+
 	rospy.Subscriber(mObj.e_gps_topic_names[0], NavSatFix, mObj.e1poseCb)
 	rospy.Subscriber(mObj.e_gps_topic_names[1], NavSatFix, mObj.e2poseCb)
 	rospy.Subscriber(mObj.e_gps_topic_names[2], NavSatFix, mObj.e3poseCb)
 
 	k=0
-	while k<1000:
+	while k<100:
 		k=k+1
 		mObj.rate.sleep()
 	
@@ -364,7 +427,10 @@ def main():
 		mObj.battle()
 		mObj.d_msg.header.stamp = rospy.Time.now()
 		mObj.e_msg.header.stamp = rospy.Time.now()
-		mObj.master_msg.header.stamp = rospy.Time.now() 
+		mObj.master_msg.header.stamp = rospy.Time.now()
+
+		mObj.update_prediciton_rate()
+
 		d_pub.publish(mObj.d_msg)
 		e_pub.publish(mObj.e_msg)
 		master_pub.publish(mObj.master_msg)
